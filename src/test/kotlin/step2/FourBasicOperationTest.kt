e2dc6b4b1813a1185725af53941e5122e422a052
changhwan <chang5hustle@gmail.com>
2024-11-13 21:29:34 +0900

    feat: 사칙연산 관련 도메인 클래스 구성 및 로직구현

--- src/test/kotlin/step2/FourBasicOperationTest.kt
+++ src/test/kotlin/step2/FourBasicOperationTest.kt
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import step2.domain.fourBasicOperations.Divide
+import step2.domain.fourBasicOperations.Minus
+import step2.domain.fourBasicOperations.Plus
+import step2.domain.fourBasicOperations.Times
 
 // 사칙연산 테스트
 class FourBasicOperationTest {
@@ -12,7 +16,7 @@ class FourBasicOperationTest {
     inner class PlusTest {
         @Test
         fun `2 + 2 는 4이다 양수 + 양수 테스트`() {
-            val operationResult = 2 + 2
+            val operationResult = Plus(2, 2).execute()
             val expected = 4
 
             assertThat(operationResult).isEqualTo(expected)
@@ -20,7 +24,7 @@ class FourBasicOperationTest {
 
         @Test
         fun `2 + -1 은 1이다 양수 + 음수 테스트 `() {
-            val operationResult = 2 + -1
+            val operationResult = Plus(2, -1).execute()
             val expected = 1
 
             assertThat(operationResult).isEqualTo(expected)
@@ -28,7 +32,7 @@ class FourBasicOperationTest {
 
         @Test
         fun `-1 + -1 은 -2이다 음수 +음수 테스트`() {
-            val operationResult = -1 + -1
+            val operationResult = Plus(-1, -1).execute()
             val expected = -2
 
             assertThat(operationResult).isEqualTo(expected)
@@ -39,7 +43,7 @@ class FourBasicOperationTest {
     inner class MinusTest {
         @Test
         fun `2 - 2 는 0이다 양수 - 양수 테스트`() {
-            val operationResult = 2 - 2
+            val operationResult = Minus(2, 2).execute()
             val expected = 0
 
             assertThat(operationResult).isEqualTo(expected)
@@ -47,7 +51,7 @@ class FourBasicOperationTest {
 
         @Test
         fun `2 - -1 은 3이다 양수 - 음수 테스트 `() {
-            val operationResult = 2 - -1
+            val operationResult = Minus(2, -1).execute()
             val expected = 3
 
             assertThat(operationResult).isEqualTo(expected)
@@ -55,7 +59,7 @@ class FourBasicOperationTest {
 
         @Test
         fun `-1 - -1 은 0이다 음수 - 음수 테스트`() {
-            val operationResult = -1 - -1
+            val operationResult = Minus(-1, -1).execute()
             val expected = 0
 
             assertThat(operationResult).isEqualTo(expected)
@@ -66,7 +70,7 @@ class FourBasicOperationTest {
     inner class TimesTest {
         @Test
         fun `2 * 2 는 4이다 양수 * 양수 테스트`() {
-            val operationResult = 2 * 2
+            val operationResult = Times(2, 2).execute()
             val expected = 4
 
             assertThat(operationResult).isEqualTo(expected)
@@ -74,7 +78,7 @@ class FourBasicOperationTest {
 
         @Test
         fun `2 * -1 은 -2이다 양수 * 음수 테스트 `() {
-            val operationResult = 2 * -1
+            val operationResult = Times(2, -1).execute()
             val expected = -2
 
             assertThat(operationResult).isEqualTo(expected)
@@ -82,7 +86,7 @@ class FourBasicOperationTest {
 
         @Test
         fun `-1 * -1 은 1이다 음수 * 음수 테스트`() {
-            val operationResult = -1 * -1
+            val operationResult = Times(-1, -1).execute()
             val expected = 1
 
             assertThat(operationResult).isEqualTo(expected)
@@ -94,7 +98,7 @@ class FourBasicOperationTest {
         @Test
         @DisplayName("2 / 2 는 1이다 양수 / 양수 테스트")
         fun `divide test 1`() {
-            val operationResult = 2 / 2
+            val operationResult = Divide(2, 2).execute()
             val expected = 1
 
             assertThat(operationResult).isEqualTo(expected)
@@ -103,7 +107,7 @@ class FourBasicOperationTest {
         @Test
         @DisplayName("2 / -1 은 -2이다 양수 / 음수 테스트")
         fun `divide test 2`() {
-            val operationResult = 2 / -1
+            val operationResult = Divide(2, -1).execute()
             val expected = -2
 
             assertThat(operationResult).isEqualTo(expected)
@@ -112,7 +116,7 @@ class FourBasicOperationTest {
         @Test
         @DisplayName("-1 / -1 은 1이다 음수 /음수 테스트")
         fun `divide test 3`() {
-            val operationResult = -1 / -1
+            val operationResult = Divide(-1, -1).execute()
             val expected = 1
 
             assertThat(operationResult).isEqualTo(expected)
@@ -121,7 +125,7 @@ class FourBasicOperationTest {
         @Test
         fun `0으로 숫자를 나눌수 없다`() {
             assertThrows(ArithmeticException::class.java) {
-                3 / 0
+                Divide(3, 0).execute()
             }
         }
     }
